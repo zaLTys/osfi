ae99daf79060534c38c3a8d6cc6b1cf0356615cc
zaLTys <povilas.simanskas@gmail.com>
2013-05-29 12:08:44 +0300

    dalinam is 1000, pradedam deliot suapvalinima

---
+++ StatistinesAtaskaitos/DecimalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StatistinesAtaskaitos
+{
+    public static class DecimalExtensions
+    {
+        public static string ToString(this decimal? value, string format)
+        {
+            if (value == null) return "";
+            return value.Value.ToString(format);
+        }
+    }
+    
+}
